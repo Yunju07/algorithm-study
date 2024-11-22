356089d572f9bcf6656c5d4f4c7a17153f332676
yunju <juile0827@naver.com>
2024-11-22 23:18:35 +0900

    자바 자주 사용하는 문법 정리 : List 관련 메소드

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,8 +1,11 @@
 // 자바 자주 사용하는 코딩 테스트 문법
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        ListMethod();
+
     }
 
     public static void StringMethod() {
@@ -72,6 +75,48 @@ public class Main {
     }
 
     public static void ListMethod() {
+        /** List 관련 메소드 **/
+        List<String> list = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        
+        // 요소 삽입
+        list.add("one");
+        list.add(0, "zero");
+        list2.add("two");
+
+        // 리스트 합치기
+        list.addAll(list2);    // list = ["zero", "one", "two"]
+
+        // 특정 요소의 인덱스 반환
+        list.indexOf("zero");           // 첫번째 인덱스
+        list.lastIndexOf("zero");    // 마지막 인덱스
+
+        // 인덱스로 요소 조회
+        list.get(2);    // "two"
+
+        // 요소 삭제
+        list.remove(0);      // 인덱스로 삭제 ["one", "two"]
+        list.remove("one");     // 첫번째 요소 삭제 ["two"]
+
+        // 리스트 차집합
+        list.removeAll(list2);      // list에서 list2와 겹치는 값 삭제
+        // 리스트 교집합
+        list.retainAll(list2);      // list에서 list2와 겹치는 값만 남기고 삭제
+
+        //리스트 비우기
+        list.clear();
+
+        // 리스트 비어있는지 체크
+        list.isEmpty();
+
+        // 리스트 길이
+        list.size();
+
+        // 특성 요소 포함 여부 체크
+        list.contains("zero");
+
+        // 리스트에 다른 리스트 요소 전부 포함 여부 체크
+        list.containsAll(list2);    // list에 list2의 모든 값이 포함되어 있으면 true
 
     }
 }
