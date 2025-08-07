c88614d31d74831e4a28a195aa3ef4da6d1da3eb
Yunju07 <juile0827@naver.com>
2025-08-07 23:44:46 +0900

    25.08.07 Dance Dance Revolution 문제풀이
    dp

--- java/src/baekjoon/BOJ2342.java
+++ java/src/baekjoon/BOJ2342.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 // DDR
-public class Main {
+public class BOJ2342 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
